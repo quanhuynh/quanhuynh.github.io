169e17a494d49bdaa26b4ad5992a8db844f118ae
Quan Huynh <quanhuynh96@berkeley.edu>
2015-08-17 18:38:37 -0700

    modified layout

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -41,8 +41,19 @@ function workLoad() {
 
 	})
 
-
 }
+
+$(window).scroll(function() {
+
+	var wScroll = $(this).scrollTop();
+
+	if(wScroll > $('.first-par').offset().top - $(window).height()){
+    var offset = Math.min(0, wScroll - $('.first-par').offset().top + $(window).height()-350);
+    $('.first-par').css({'transform': 'translate('+offset+'px, '+ -offset*0.5 +'px)'});
+    $('.second-par').css({'transform': 'translate('+Math.abs(offset)+'px, '+Math.abs(offset*0.5)+'px)'});
+  }
+
+})
 
 //implementation of nicescroll.js
 $(document).ready(
